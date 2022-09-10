3a0ad9c3107765f4581867c3cd1242cecc084337
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-10 16:18:48 -0400

    Added template for function comments.

--- game.js
+++ game.js
@@ -1,6 +1,13 @@
 /******************************************************************************
-          Name: game.js
-        Author: Chad Chapman
-  Date Created: September 10, 2022
-   Description: Implementation for Rock Paper Scissors game
+ *         Name: game.js
+ *       Author: Chad Chapman
+ * Date Created: September 10, 2022
+ *  Description: Implementation for Rock Paper Scissors game
 ******************************************************************************/
+
+/******************************************************************************
+ *        Name:
+ * Description:
+ *   Arguments:
+ *      Params:
+ *****************************************************************************/
