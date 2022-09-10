d05eb13ae246267bfde4c5aefabddd8cf8a60c0f
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-10 16:16:50 -0400

    Added file description comments.

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+/******************************************************************************
+          Name: game.js
+        Author: Chad Chapman
+  Date Created: September 10, 2022
+   Description: Implementation for Rock Paper Scissors game
+******************************************************************************/
