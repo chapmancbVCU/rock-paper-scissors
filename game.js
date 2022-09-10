c441840788ee62bd58664ef18e3fec15be51f859
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-10 16:26:01 -0400

    Create function definitions.

--- game.js
+++ game.js
@@ -2,12 +2,64 @@
  *         Name: game.js
  *       Author: Chad Chapman
  * Date Created: September 10, 2022
- *  Description: Implementation for Rock Paper Scissors game
+ *  Description: Implementation for Rock Paper Scissors game.
 ******************************************************************************/
+
+/******************************************************************************
+ *        Name: getComputerChoice
+ * Description: Randomly sets choice for computer.
+ *   Arguments:
+ *     Returns:
+ *****************************************************************************/
+function getComputerChoice (){
+
+}
+
+
+
+
+
+/******************************************************************************
+ *        Name: playRound
+ * Description: Implements each round for the game.
+ *   Arguments: playerSelection - selection for human player
+ *   Arguments: computerSelection
+ *     Returns:
+ *****************************************************************************/
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+/******************************************************************************
+ *        Name: game
+ * Description: Main execution section for the game Rock Paper Scissors
+ *   Arguments:
+ *     Returns:
+ *****************************************************************************/
+function game()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /******************************************************************************
  *        Name:
  * Description:
  *   Arguments:
- *      Params:
+ *     Returns:
  *****************************************************************************/
