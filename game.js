b56c1013c79f7db2e2f3fbf5b42051872cc2163f
Chad Chapman <chad.chapman2010@gmail.com>
2022-09-10 16:49:31 -0400

    Created way to verify if input is correct.

--- game.js
+++ game.js
@@ -34,16 +34,45 @@ function playRound(playerSelection, computerSelection) {
 /******************************************************************************
  *        Name: game
  * Description: Main execution section for the game Rock Paper Scissors
- *   Arguments:
- *     Returns:
+ *   Arguments: NONE
+ *     Returns: NONE
  *****************************************************************************/
 function game()
 {
+    // Number of rounds for this game.
+    let rounds = 5;
 
+    // Main execution loop for the 5 rounds.
+    for(let i = 0; i < rounds; i++) {
+        
+        // Boolean flag to break out of loop once valid choice is selected.
+        let isValidChoice = false;
+
+        let choice;
+        // Determine if input is valid in this do while loop.
+        do {
+            // Ask user to enter a choice for the game.
+            choice = prompt(`Enter rock, paper, or scissors (all lower case) for round ${i + 1}:`, "");
+
+            // Verify choice and alert user if it is invalid.
+            if(choice === "rock" || choice === "paper" || choice === "scissors") {
+                isValidChoice = true;
+            }
+            else {
+                alert("You entered an invalid choice!");
+                isValidChoice = false;
+            }
+        } while (isValidChoice == false);
+
+        // Once a valid choice is made we alert the user.
+        alert(`You entered "${choice}"`);
+    }
 }
 
-
-
+/******************************************************************************
+ * Main
+ *****************************************************************************/
+game();
 
 
 
